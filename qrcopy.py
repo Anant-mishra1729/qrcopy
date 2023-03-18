46956d70f91680f402be0e6943e186a001727021
Anant Mishra <84588156+Anant-mishra1729@users.noreply.github.com>
2023-03-18 19:23:41 +0530

    Update qrcopy.py

--- qrcopy.py
+++ qrcopy.py
@@ -1,8 +1,3 @@
-#!/home/{USER_NAME}/miniconda3/envs/{ENVIRONMENT NAME}/bin/python
-# Replace "USER NAME" with your user name and "ENVIRONMENT NAME" with your environment name
-# OR 
-# Type "which python" to get your python executable path replace it with path in first line
-
 import qrcode
 from PIL import Image 
 from tkinter import Tk
