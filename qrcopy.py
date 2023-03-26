1ec6de7c3500b38bbca308f69fad0ad4c0ecb98f
rk <rk@pop-os.localdomain>
2023-03-26 18:30:17 +0530

    Integrated Pastebin class

--- qrcopy.py
+++ qrcopy.py
@@ -3,7 +3,28 @@ import pyperclip
 import argparse
 import os
 import json
-from utils.pastebin import Pastebin
+import requests
+
+class Pastebin:
+    def __init__(self, api_key, api_option='paste', api_paste_private='0', api_paste_expire_date='N', api_paste_format='text'):
+        self.api_key = api_key
+        self.url = 'https://pastebin.com/api/api_post.php'
+        self.headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        self.data = {
+            'api_dev_key': self.api_key, # api key
+            'api_option': api_option, # Available options: paste, list, login, userdetails, delete, expire etc.
+            'api_paste_private': api_paste_private, # 0 = public, 1 = unlisted, 2 = private
+            'api_paste_expire_date': api_paste_expire_date, # N = Never, 10M = 10 Minutes, 1H = 1 Hour, 1D = 1 Day, 1W = 1 Week, 2W = 2 Weeks, 1M = 1 Month, 6M = 6 Months, 1Y = 1 Year
+            'api_paste_format': api_paste_format # Available formats: text, c, cpp, csharp, css, diff, html4strict, java, javascript, lua, mysql, objectivec, perl, php, python, ruby, vb, xml
+        }
+    def upload(self, text, title):
+        self.data['api_paste_code'] = text
+        self.data['api_paste_name'] = title
+        r = requests.post(self.url, headers=self.headers, data=self.data)
+        return r.text
+    
+    def get_url(self, text, title):
+        return self.upload(text, title)
 
 def upload_to_pastebin(text):
     # Read config file for pastebin api key
