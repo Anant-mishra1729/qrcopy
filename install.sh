c6f0e4bd8b14e5fe211cf06b1a6fbb58028eb90d
rk <rk@pop-os.localdomain>
2023-03-26 17:36:52 +0530

    Some changes

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 QR_COPY_DIR=~/.local/qrcopy
 
 # Config file to store installation details
-echo -e "\e[1m\e[32mCreating config file...\e[0m"
+echo -e "\e[1m\e[32mCreating config file...\n\e[0m"
 cat << EOF > "$HOME/.config/qrcopy/qrcopy.json"
 {
   "QRCOPY_DIR": "$QR_COPY_DIR",
@@ -19,11 +19,11 @@ if [ ! -d "$QR_COPY_DIR" ]; then
 fi
 
 # download the qrcopy.py file from GitHub using curl
-echo -e "\e[1m\e[32mDownloading qrcopy.py...\e[0m"
+echo -e "\e[1m\e[32mDownloading qrcopy.py...\n\e[0m"
 curl -o "$QR_COPY_DIR/qrcopy.py" https://raw.githubusercontent.com/Anant-mishra1729/qrcopy/main/qrcopy.py
 
 # create the qrcopy.sh file
-echo -e "\e[1m\e[32mCreating qrcopy.sh file...\e[0m"
+echo -e "\e[1m\e[32mCreating qrcopy.sh file...\n\e[0m"
 
 cat << EOF > "$QR_COPY_DIR/qrcopy.sh"
 #!/bin/bash
@@ -32,20 +32,20 @@ python qrcopy.py "$@"
 EOF
 
 # set the permission for the qrcopy.py and qrcopy.sh script
-echo -e "\e[1m\e[32mSetting permissions for scripts...\e[0m"
+echo -e "\e[1m\e[32mSetting permissions for scripts...\n\e[0m"
 chmod +x "$QR_COPY_DIR/qrcopy.py"
 chmod +x "$QR_COPY_DIR/qrcopy.sh"
 
 # create a virtual environment named qrcopy using python3-venv
-echo -e "\e[1m\e[32mCreating virtual environment for qrcopy...\e[0m"
+echo -e "\e[1m\e[32mCreating virtual environment for qrcopy...\n\e[0m"
 python3 -m venv "$QR_COPY_DIR/qrcopy"
 
 # activate the qrcopy virtual environment
-echo -e "\e[1m\e[32mActivating virtual environment...\e[0m"
+echo -e "\e[1m\e[32mActivating virtual environment...\n\e[0m"
 source "$QR_COPY_DIR/qrcopy/bin/activate"
 
 # install the required packages using pip
-echo -e "\e[1m\e[32mInstalling required packages using pip...\e[0m"
+echo -e "\e[1m\e[32mInstalling required packages using pip...\n\e[0m"
 pip install qrcode Pillow
 
 # prompt the user to enter their Pastebin API key
@@ -56,11 +56,11 @@ sed -i "s/^PASTEBIN_API_KEY=.*/PASTEBIN_API_KEY=\"$PASTEBIN_API_KEY\"/" "$QR_COP
 
 # set an alias for qrcopy in the .bashrc or .bash_aliases file
 if [ -f ~/.bash_aliases ]; then
-  echo -e "\e[1m\e[32mAdding alias to .bash_aliases...\e[0m"
+  echo -e "\e[1m\e[32mAdding alias to .bash_aliases...\n\e[0m"
   echo "alias qrcopy='$QR_COPY_DIR/qrcopy.sh'" >> ~/.bash_aliases
   source ~/.bash_aliases
 else
-  echo -e "\e[1m\e[32mAdding alias to .bashrc...\e[0m"
+  echo -e "\e[1m\e[32mAdding alias to .bashrc...\n\e[0m"
   echo "alias qrcopy='$QR_COPY_DIR/qrcopy.sh'" >> ~/.bashrc
   source ~/.bashrc
 fi
