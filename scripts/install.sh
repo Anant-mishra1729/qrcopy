794b6aab412cc018b54ae03236ffdb94ff645c2d
Anant Mishra <84588156+Anant-mishra1729@users.noreply.github.com>
2023-04-24 20:34:07 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -56,7 +56,7 @@ source "$QR_COPY_DIR/qrcopy/bin/activate"
 echo -e "\e[1m\e[32mInstalling required packages using pip...\n\e[0m"
 pip install qrcode Pillow pyperclip requests
 
-echo -e "\e[1m\e[36mEnter your Pastebin API key in ~/.config/qrcopy/qrcopy.conf (leave blank if you don't have one): \e[0m"
+echo -e "\e[1m\e[36mEnter your Pastebin API key in ~/.config/qrcopy/qrcopy.conf (You can get it from pastebin.com) \e[0m"
 
 # set an alias for qrcopy in the .bashrc or .bash_aliases file
 if [ -f ~/.bash_aliases ]; then
