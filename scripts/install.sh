749aa149f5b774eb2a8415a6d77b59d1769b581b
Anant Mishra <84588156+Anant-mishra1729@users.noreply.github.com>
2023-04-24 20:14:31 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 # check if python3-venv is installed
 if ! command -v python3-venv &> /dev/null
 then
-    echo -e "\e[31mpython3-venv\e[0m could not be found. Please install python3-venv before running this script."
+    echo -e "python3-venv could not be found. Please install \e[31mpython3-venv\e[0m before running this script."
     exit 1
 fi
 
